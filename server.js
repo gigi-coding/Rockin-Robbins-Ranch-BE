2cd0c9608effbd12e97980e5c6526ca0b634470c
gigi-coding <brigitte.d.quintero@gmail.com>
2022-06-06 14:36:15 -0600

    mongoose connection established

--- server.js
+++ server.js
@@ -5,8 +5,19 @@
 require("dotenv").config();
 const { PORT = 3000, MONGODB_URL } = process.env;
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+// Establish Connection
+mongoose.connect(MONGODB_URL);
+// Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to mongoose.."))
+.on("close", () => console.log("You are disconnected from mongoose.."))
+.on("error", (error) => console.log(error));
 
 ///////////////////////////////
 // ROUTES
